55b22766bc53474c0cb92e027d52ad77c445852d
Caio Bomfim <caaiobomfim@gmail.com>
2025-07-20 13:01:26 -0300

    :rocket: feat: add code to be refactored with SRP

--- single-responsibility-principle/src/main/java/caaiobomfim/application/SolidApplication.java
+++ single-responsibility-principle/src/main/java/caaiobomfim/application/SolidApplication.java
@@ -2,6 +2,8 @@ package caaiobomfim.application;
 
 public class SolidApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Order order = new Order("ba1aef0d-4ccb-489c-90b5-cb7664a53c2d", 500);
+        OrderProcessorService orderProcessorService = new OrderProcessorService();
+        orderProcessorService.processOrder(order);
     }
 }
